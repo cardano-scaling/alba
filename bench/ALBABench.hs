1b8a0f3aa87afdcb310ad7dcb92bf2baba8051a3
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-04-26 15:54:12 +0200

    Fix benchmarks code and parameters

--- bench/ALBABench.hs
+++ bench/ALBABench.hs
@@ -1,17 +1,17 @@
+import ALBA (Bytes (..), Params (..), Proof, hash, prove)
 import Control.Monad (forM)
 import Criterion (Benchmark)
 import Criterion.Main (bench, bgroup, defaultMain, whnf)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Word (Word8)
-import Peras.ALBA (Bytes (..), Params (..), Proof, hash, prove)
 import Test.QuickCheck (Gen, arbitrary, generate, vectorOf)
 
 main :: IO ()
 main = do
   benches <-
     forM
-      [ (s_p, len) | s_p <- [100, 200, 300, 400], len <- [64, 128, 256]
+      [ (s_p, len) | s_p <- [100, 200, 300], len <- [64, 128, 256]
       ]
       genItems
   defaultMain
@@ -20,7 +20,8 @@ main = do
         $ map benchHash (filter ((== 256) . BS.length . head) benches)
     , bgroup
         "Proof Generation"
-        $ map benchProof benches
+        -- code does not currently run in a reasonable time for larger values of λ
+        $ [benchProof (b, λ) | b <- benches, λ <- [8, 9, 10]]
     ]
 
 benchHash :: [ByteString] -> Benchmark
@@ -29,10 +30,10 @@ benchHash bytes =
  where
   label = "hashing " <> show (length bytes) <> " strings, len=" <> show (BS.length $ head bytes)
 
-benchProof :: [ByteString] -> Benchmark
-benchProof bytes =
+benchProof :: ([ByteString], Integer) -> Benchmark
+benchProof (bytes, λ) =
   let coeff = fromIntegral $ length bytes
-      params = Params 8 8 (coeff * 8 `div` 10) (coeff * 2 `div` 10)
+      params = Params λ λ (coeff * 8 `div` 10) (coeff * 2 `div` 10)
       label = "Proving " <> show coeff <> "/" <> show (BS.length $ head bytes) <> " (Params: " <> show params <> ")"
    in bench label $
         whnf
