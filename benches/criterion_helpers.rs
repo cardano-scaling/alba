14a2a3b830cd0202cd8e4baccc503aed41e3c7ab
Raphael Toledo <raphael.toledo@iohk.io>
2024-10-08 12:23:28 +0100

    Adding disabled and broken repetition bench

--- benches/criterion_helpers.rs
+++ benches/criterion_helpers.rs
@@ -103,3 +103,87 @@ impl ValueFormatter for StepsFormatter {
         "steps"
     }
 }
+
+/// Nb of repet, times prove_index was called, per proof
+///
+pub struct Repetitions;
+impl Measurement for Repetitions {
+    type Intermediate = u64;
+    type Value = u64;
+
+    fn start(&self) -> Self::Intermediate {
+        0
+    }
+
+    fn end(&self, _i: Self::Intermediate) -> Self::Value {
+        0
+    }
+
+    fn add(&self, v1: &Self::Value, v2: &Self::Value) -> Self::Value {
+        v1 + v2
+    }
+
+    fn zero(&self) -> Self::Value {
+        0
+    }
+
+    fn to_f64(&self, value: &Self::Value) -> f64 {
+        *value as f64
+    }
+
+    fn formatter(&self) -> &dyn ValueFormatter {
+        &RepetitionsFormatter
+    }
+}
+
+struct RepetitionsFormatter;
+
+impl ValueFormatter for RepetitionsFormatter {
+    fn format_value(&self, value: f64) -> String {
+        format!("{:.4} repet", value)
+    }
+
+    fn format_throughput(&self, throughput: &Throughput, value: f64) -> String {
+        match throughput {
+            Throughput::Bytes(b) => format!("{:.4} rpb", value / *b as f64),
+            Throughput::Elements(b) => format!("{:.4} repet/{}", value, b),
+            Throughput::BytesDecimal(b) => format!("{:.4} rpb (decimal)", value / *b as f64),
+        }
+    }
+
+    fn scale_values(&self, _typical_value: f64, _values: &mut [f64]) -> &'static str {
+        "repet"
+    }
+
+    fn scale_throughputs(
+        &self,
+        _typical_value: f64,
+        throughput: &Throughput,
+        values: &mut [f64],
+    ) -> &'static str {
+        match throughput {
+            Throughput::Bytes(n) => {
+                for val in values {
+                    *val /= *n as f64;
+                }
+                "rpb"
+            }
+            Throughput::Elements(n) => {
+                for val in values {
+                    *val /= *n as f64;
+                }
+                "rpe"
+            }
+            Throughput::BytesDecimal(n) => {
+                for val in values {
+                    *val /= *n as f64;
+                }
+                "rpb (decimal)"
+            }
+        }
+    }
+
+    fn scale_for_machines(&self, _values: &mut [f64]) -> &'static str {
+        "repet"
+    }
+}
