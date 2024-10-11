74eda3ddc2ccee032ee1f4dca0b00c51151156db
Raphael Toledo <raphael.toledo@iohk.io>
2024-10-11 10:10:04 +0100

    Adding second oracle

--- src/utils.rs
+++ src/utils.rs
@@ -2,9 +2,37 @@ use blake2::digest::{Update, VariableOutput};
 use blake2::Blake2bVar;
 use std::cmp::min;
 
-/// Binary Random Oracles (c.f. Appendix B, Alba paper)
-/// Takes as input a hash and inverse probability and returns an integer
-pub fn oracle(hash: &[u8], n: usize) -> usize {
+// Oracles
+
+/// Takes as input a hash and inverse probability $np$ and returns a
+/// uniformly distributed integer in [1, np] (c.f. Appendix B, Alba paper).
+pub fn oracle_uniform(hash: &[u8], n: usize) -> usize {
+    // Computes the integer reprensation of hash* modulo n when n is not a
+    // power of two. *(up to 8 bytes, in little endian)
+    fn mod_non_power_of_2(hash: &[u8], n: usize) -> usize {
+        fn log_base2(x: usize) -> usize {
+            usize::BITS as usize - x.leading_zeros() as usize - 1
+        }
+        let epsilon_fail: usize = 1 << 40; // roughly 1 trillion
+        let k: usize = log_base2(n * epsilon_fail);
+        let k_prime: usize = 1 << k;
+        let d: usize = k_prime.div_ceil(n);
+
+        let i = mod_power_of_2(hash, k_prime);
+
+        if i >= d * n {
+            panic!("failed: i = {}, d = {}, n = {}, k = {}", i, d, n, k);
+        } else {
+            i % n
+        }
+    }
+    // Computes the integer reprensation of hash* modulo n when n is a power of
+    // two. *(up to 8 bytes, in little endian)
+    fn mod_power_of_2(hash: &[u8], n: usize) -> usize {
+        assert!(8 * hash.len() >= (n as f32).log2() as usize);
+        from_bytes_le(hash) & (n - 1)
+    }
+
     if n.is_power_of_two() {
         mod_power_of_2(hash, n)
     } else {
@@ -12,33 +40,36 @@ pub fn oracle(hash: &[u8], n: usize) -> usize {
     }
 }
 
-fn mod_non_power_of_2(hash: &[u8], n: usize) -> usize {
-    fn log_base2(x: usize) -> usize {
-        usize::BITS as usize - x.leading_zeros() as usize - 1
+/// Takes as input a hash and probability $q$ and returns true with
+/// probability q otherwise false according to a Binomial distribution
+/// (c.f. Appendix B, Alba paper).
+pub fn oracle_binomial(hash: &[u8], q: f64) -> bool {
+    // For error parameter ɛ̝, find an approximation x/y of q with (x,y) in N²
+    // such that 0 < q - x/y <= ɛ̝
+    let epsilon_fail: usize = 1 << 40; // roughly 1 trillion
+    let mut x: usize = q.ceil() as usize;
+    let mut y: usize = 1;
+    while {
+        let difference = q - (x as f64 / y as f64);
+        difference >= 1.0 / epsilon_fail as f64 || difference < 0.0
+    } {
+        y = y * 2;
+        x = (q * (y as f64)).round() as usize;
     }
-    let epsilon_fail: usize = 1 << 40; // roughly 1 in 10 billion
-    let k: usize = log_base2(n * epsilon_fail);
-    let k_prime: usize = 1 << k;
-    let d: usize = k_prime.div_ceil(n);
-
-    let i = mod_power_of_2(hash, k_prime);
-
-    if i >= d * n {
-        panic!("failed: i = {}, d = {}, n = {}, k = {}", i, d, n, k);
-    } else {
-        i % n
-    }
+    // Output i in [0; y-1] from hash
+    assert!(8 * hash.len() >= (y as f32).log2() as usize);
+    let i = from_bytes_le(&hash) & (y - 1);
+    // Return true if i < x
+    i < x
 }
 
-fn mod_power_of_2(hash: &[u8], n: usize) -> usize {
-    fn from_bytes_le(bytes: &[u8]) -> usize {
-        let mut array = [0u8; 8];
-        let bytes = &bytes[..min(8, bytes.len())];
-        array[..bytes.len()].copy_from_slice(bytes);
-        usize::from_le_bytes(array)
-    }
-    let r = from_bytes_le(hash);
-    (n - 1) & r
+// Returns the integer representation of, up to the 8 first bytes of, the
+// input bytes in little endian
+fn from_bytes_le(bytes: &[u8]) -> usize {
+    let mut array = [0u8; 8];
+    let bytes = &bytes[..min(8, bytes.len())];
+    array[..bytes.len()].copy_from_slice(bytes);
+    usize::from_le_bytes(array)
 }
 
 // Hash helpers
