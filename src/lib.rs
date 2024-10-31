2119a55fbb20c40c553c6ba1464ec59f0d0f0a7b
Raphael Toledo <raphael.toledo@iohk.io>
2024-10-31 12:12:59 +0000

    Fixing visibility

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 //! An implementation of Approximate Lower Bound Arguments
 //! (ALBA, <https://eprint.iacr.org/2023/1655.pdf>).
-pub mod test_utils;
-pub mod utils;
+#[cfg(test)]
+mod test_utils;
+
+mod utils;
 
 pub mod centralized_telescope;
