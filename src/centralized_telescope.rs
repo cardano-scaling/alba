fb03448215726292c977ac862e3e661586584624
Raphael Toledo <raphael.toledo@iohk.io>
2024-10-31 10:31:57 +0000

    f64::log2_e

--- src/centralized_telescope.rs
+++ src/centralized_telescope.rs
@@ -3,7 +3,7 @@
 
 use crate::utils;
 use blake2::{Blake2s256, Digest};
-use std::{f32::consts::LOG2_E, f64::consts::E};
+use std::f64::consts::{E, LOG2_E};
 
 const DATA_LENGTH: usize = 32;
 const DIGEST_SIZE: usize = 32;
@@ -58,17 +58,16 @@ impl Setup {
     }
 
     fn param_high_case(params: &Params, u_f64: f64, lambda_rel2: f64, sec_param: u64) -> Self {
-        let loge = f64::from(LOG2_E);
         let l2 = lambda_rel2 + 2.0;
-        let d = (16.0 * u_f64 * l2 / loge).ceil();
-        debug_assert!(params.n_p as f64 >= d * d * loge / (9.0 * l2));
+        let d = (16.0 * u_f64 * l2 / LOG2_E).ceil();
+        debug_assert!(params.n_p as f64 >= d * d * LOG2_E / (9.0 * l2));
         Self {
             sec_param,
             n_p: params.n_p,
             u: u_f64 as u64,
             r: (params.lambda_rel / lambda_rel2).ceil() as u64,
             d: d as u64,
-            q: 2.0 * l2 / (d * loge),
+            q: 2.0 * l2 / (d * LOG2_E),
             b: (((l2 + u_f64.log2()) / l2) * (3.0 * u_f64 * d / 4.0) + d + u_f64).floor() as u64,
         }
     }
@@ -97,7 +96,7 @@ impl Setup {
             w
         }
         let lambda_rel1 = params.lambda_rel.min(s1);
-        let lbar = (lambda_rel1 + 7.0) / f64::from(LOG2_E);
+        let lbar = (lambda_rel1 + 7.0) / LOG2_E;
         let d = (16.0 * u_f64 * lbar).ceil();
         let lbar_over_d = lbar / d;
         debug_assert!(params.n_p as f64 >= d * d / (9.0 * lbar));
@@ -120,15 +119,14 @@ impl Setup {
         let sec_param = params.lambda_rel.max(params.lambda_sec).ceil() as u64;
         let n_p_f64 = params.n_p as f64;
         let n_f_f64 = params.n_f as f64;
-        let loge = f64::from(LOG2_E);
 
         let u_f64 = {
-            let numerator = params.lambda_sec + params.lambda_rel.log2() + 5.0 - loge.log2();
+            let numerator = params.lambda_sec + params.lambda_rel.log2() + 5.0 - LOG2_E.log2();
             let denominator = (n_p_f64 / n_f_f64).log2();
             (numerator / denominator).ceil()
         };
 
-        let ratio = 9.0 * n_p_f64 * loge / ((17.0 * u_f64).powi(2));
+        let ratio = 9.0 * n_p_f64 * LOG2_E / ((17.0 * u_f64).powi(2));
         let s1 = ratio - 7.0;
         let s2 = ratio - 2.0;
 
