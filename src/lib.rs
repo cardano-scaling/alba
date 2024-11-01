d98d9e89048009d9f4146158b3f4f789070a6080
Raphael <raphael.r.toledo@gmail.com>
2024-11-01 16:37:34 +0000

    Adding centralised alba (bounded scheme) (#33)
    ## Content
    Adding Bounded scheme (section 3.2.2 ) implementation in Rust with
    randomized tests and benches.
    
    This PR includes...
    
    ## Pre-submit checklist
    
    - Branch
        - [x] Tests are provided (if possible)
        - [x] Commit sequence broadly makes sense
        - [x] Key commits have useful messages
    - PR
        - [x] No clippy warnings in the CI
        - [x] Self-reviewed the diff
        - [x] Useful pull request description
        - [x] Reviewer requested
    - Documentation
        - [ ] Update README file (if relevant)
        - [ ] Update documentation website (if relevant)
    
    ## Comments
    Repetition bench doesn't work so is disabled
    <!-- Some optional comments about the PR, such as how to run a command,
    or warnings about usage, .... -->
    
    ## Issue(s)
    Issue #25

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,8 @@
 //! An implementation of Approximate Lower Bound Arguments
 //! (ALBA, <https://eprint.iacr.org/2023/1655.pdf>).
+#[cfg(test)]
+mod test_utils;
+
+mod utils;
+
+pub mod centralized_telescope;
