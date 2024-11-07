3ece1651f410c62b56e9948d4a6b8513c6d8dd37
curiecrypt <gamze.kilic@iohk.io>
2024-11-07 18:00:40 +0300

    rebase - restructured

--- src/simple_lottery/setup.rs
+++ src/simple_lottery/setup.rs
@@ -1,7 +1,7 @@
 //! ALBA Simple lottery setup parameters.
 
 /// Setup output parameters
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Setup {
     /// Proof size (in Sp elements)
     pub u: u64,
