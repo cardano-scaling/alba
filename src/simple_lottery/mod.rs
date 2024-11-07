fc116029759e434877745ce641ccdb895b61cf07
curiecrypt <36852463+curiecrypt@users.noreply.github.com>
2024-11-07 17:49:48 +0300

    Update src/simple_lottery/mod.rs
    Co-authored-by: Tolik Zinovyev <aszinovyev@gmail.com>

--- src/simple_lottery/mod.rs
+++ src/simple_lottery/mod.rs
@@ -1,4 +1,4 @@
-//! Simple lottery construction for the Telescope ALBA  scheme.
+//! Simple lottery construction for the Telescope ALBA scheme.
 //! Covering the Section 4.1 of the paper.
 
 pub mod setup;
