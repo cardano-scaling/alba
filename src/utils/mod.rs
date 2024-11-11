c80303112f5e6206b9ca06433b1ae02e35939c4b
Raphael Toledo <raphael.toledo@iohk.io>
2024-11-11 17:01:43 +0000

    Adding ToBytes trait

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,6 +1,8 @@
+//! Shared functions, traits and test helpers
+
 pub(crate) mod sample;
 
-pub(crate) mod types;
+pub mod types;
 
 #[cfg(test)]
 pub(crate) mod test_utils;
--- src/utils/types.rs
+++ src/utils/types.rs
@@ -1,3 +1,39 @@
-pub(crate) const DATA_LENGTH: usize = 32;
+//! Trait to make ALBA input hashable via its bytes representation
 
-pub(crate) type Element = [u8; DATA_LENGTH];
+use std::mem;
+
+/// Trait to represent input as bytes
+pub trait ToBytes {
+    /// Reference on unsized array of u8
+    type ByteArray: AsRef<[u8]>;
+    /// Returns the byte representation of the element
+    fn to_be_bytes(&self) -> Self::ByteArray;
+}
+
+macro_rules! impl_ToBytes {
+    (for $($t:ty),+) => {
+        $(impl ToBytes for $t {
+            type ByteArray = [u8; mem::size_of::<Self>()];
+
+            fn to_be_bytes(&self) -> Self::ByteArray {
+                return Self::to_be_bytes(*self);
+            }
+        })*
+    }
+}
+
+impl_ToBytes!(for u8, u16, u32, u64, u128);
+impl_ToBytes!(for i8, i16, i32, i64, i128);
+
+macro_rules! impl_u8ToBytes {
+    (for $($t:ty),+) => {
+        $(impl ToBytes for $t {
+            type ByteArray = Self;
+
+            fn to_be_bytes(&self) -> Self::ByteArray {
+                *self
+            }
+        })*
+    }
+}
+impl_u8ToBytes!(for [u8;32], [u8;64], [u8;128]);
