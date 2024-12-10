db50aa3882740aee0cff06e176886c96f157fb5e
Tolik Zinovyev <tolik@bu.edu>
2024-12-10 11:39:18 -0500

    Implement simple lottery.

--- src/simple_lottery/wrapper.rs
+++ src/simple_lottery/wrapper.rs
@@ -5,7 +5,7 @@ use super::proof::Proof;
 use super::setup::Setup;
 use crate::utils::types::Element;
 
-/// The main centralized Telescope struct with prove and verify functions.
+/// The main simple lottery struct with prove and verify functions.
 #[derive(Debug, Clone, Copy)]
 pub struct Wrapper {
     setup: Setup,
@@ -24,13 +24,11 @@ impl Wrapper {
         Self { setup: *setup }
     }
 
-    /// Alba's proving algorithm, based on a depth-first search algorithm.
     /// Returns either a `Proof` or `None` if no proof is found.
     pub fn prove(&self, prover_set: &[Element]) -> Option<Proof> {
         algorithm::prove(&self.setup, prover_set)
     }
 
-    /// Alba's verification algorithm.
     /// Returns true if and only if the proof is successfully verified.
     pub fn verify(&self, proof: &Proof) -> bool {
         algorithm::verify(&self.setup, proof)
