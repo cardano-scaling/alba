b902d4b8035aace6d0b2affa7c680126709d7f3d
Raphael Toledo <raphael.toledo@iohk.io>
2024-12-12 14:00:04 +0000

    Benches/centralized_telescope: Adding step bench

---
+++ benches/centralized_telescope/number_steps.rs
@@ -0,0 +1,66 @@
+//! Benchmarking the number of DFS calls, aka number of steps, of the
+//! Centralized Telescope scheme
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use rand_chacha::ChaCha20Rng;
+use rand_core::SeedableRng;
+use std::time::Duration;
+
+mod utils;
+use utils::{
+    common::{
+        criterion_helpers::{
+            centralized::{benchmarks, BenchParam},
+            Steps,
+        },
+        test_vectors::centralized::ALL_TESTS,
+    },
+    setup, NAME,
+};
+
+use alba::centralized_telescope::algorithm::bench;
+
+/// Function benchmarking n times the number of DFS calls, aka steps
+#[allow(clippy::unit_arg)]
+fn prove_steps(param: &BenchParam, truncate_size: u64, n: u64) -> u64 {
+    let mut rng = ChaCha20Rng::from_entropy();
+    let mut total_steps = 0u64;
+    for _ in 0..n {
+        // Setup
+        let (mut dataset, telescope) = setup(&mut rng, param);
+        // Truncate the dataset to give truncate_size elements to the prover
+        dataset.truncate(truncate_size as usize);
+        // Bench the number of steps/DFS calls while generating a proof
+        black_box({
+            let steps = bench(&telescope.setup, &dataset).0;
+            total_steps = total_steps.saturating_add(steps);
+        });
+    }
+    total_steps
+}
+
+/// Run step benchmarks on list of parameters, varying the dataset the prover
+/// generates a proof from. More particularly we change
+/// - the dataset elements,
+/// - the dataset cardinality, between set_size and set_card.
+fn step_benches(c: &mut Criterion<Steps>) {
+    benchmarks::<u64, u64, Steps>(
+        c,
+        ALL_TESTS,
+        format!("{} - {}", NAME, "Steps"),
+        "Prove",
+        &prove_steps,
+    );
+}
+
+mod criterion_group {
+    #![allow(missing_docs)]
+    use super::{criterion_group, step_benches, Criterion, Duration, Steps};
+
+    criterion_group!(name = centralized_step;
+        config = Criterion::default().with_measurement(Steps).measurement_time(Duration::from_secs(30));
+        targets = step_benches
+    );
+}
+
+criterion_main!(criterion_group::centralized_step);
