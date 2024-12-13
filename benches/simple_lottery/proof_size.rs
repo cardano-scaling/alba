178dcd4ccaa54efdc56b0fe340483a4fa0e6a9a7
Raphael Toledo <raphael.toledo@iohk.io>
2024-12-13 11:06:55 +0000

    Benches/lottery: Adding proof size

---
+++ benches/simple_lottery/proof_size.rs
@@ -0,0 +1,44 @@
+//! Benchmarking the proof size of the Simple Lottery scheme
+
+use alba::simple_lottery::{init, params::Params};
+
+mod utils;
+use utils::common::{
+    criterion_helpers::centralized::BenchParam, test_vectors::centralized::ALL_TESTS,
+};
+
+/// Function benchmarking the proof size
+fn proof_bench(params: &[BenchParam]) {
+    println!("Simple Lottery -- proof size");
+    println!(
+        "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10}",
+        "soundness_param (λ_sec)",
+        "completeness_param (λ_rel)",
+        "set_size (n_p)",
+        "lower_bound (n_f)",
+        "proof_size (u)",
+        "lottery_probability (q)"
+    );
+    for param in params {
+        let params = Params {
+            soundness_param: param.lambda_sec,
+            completeness_param: param.lambda_rel,
+            set_size: param.set_size_per,
+            lower_bound: param.lower_bound_per,
+        };
+        let setup = init::make_setup(&params);
+        println!(
+            "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10}",
+            param.lambda_sec,
+            param.lambda_rel,
+            param.set_size_per,
+            param.lower_bound_per,
+            setup.proof_size,
+            setup.lottery_probability
+        );
+    }
+}
+
+fn main() {
+    proof_bench(ALL_TESTS);
+}
