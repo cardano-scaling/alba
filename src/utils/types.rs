170876ad0eb7b2513b8e300ca4d36f5d3ce52296
Raphael Toledo <raphael.toledo@iohk.io>
2024-12-13 12:59:36 +0000

    Src/Moving Hash type to crate::utils

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -1,3 +1,10 @@
 pub(crate) const DATA_LENGTH: usize = 48;
 
+/// Type of dataset's elements to lower bound
 pub(crate) type Element = [u8; DATA_LENGTH];
+
+/// Digest size for internal hashes
+pub(crate) const DIGEST_SIZE: usize = 32;
+
+/// Hash type for internal hashes
+pub(crate) type Hash = [u8; DIGEST_SIZE];
