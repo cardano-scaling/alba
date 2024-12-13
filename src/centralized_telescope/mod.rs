74070a5b8c78570934b361470226db9f5a928adb
Raphael Toledo <raphael.toledo@iohk.io>
2024-12-13 11:53:18 +0000

    CentralizedTelescope: Adding cases

--- src/centralized_telescope/mod.rs
+++ src/centralized_telescope/mod.rs
@@ -4,6 +4,7 @@
 #![doc = include_str!("../../docs/rustdoc/centralized_telescope/intro.md")]
 //!
 //! See full documentation: [Telescope - Construction with Bounded DFS][crate::docs::centralized]
+mod cases;
 
 pub mod params;
 
