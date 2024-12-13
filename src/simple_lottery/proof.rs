1a4f90c9a1eec85dae626204819c2234ead365a7
Raphael Toledo <raphael.toledo@iohk.io>
2024-12-13 13:53:58 +0000

    SimpleLottery: Moving algo.rs in proof.rs implementation block

--- src/simple_lottery/proof.rs
+++ src/simple_lottery/proof.rs
@@ -1,10 +1,47 @@
 //! Simple lottery Proof structure
 
-use crate::utils::types::Element;
+use super::params::Params;
+use crate::utils::{
+    sample,
+    types::{Element, Hash},
+};
+use blake2::{Blake2s256, Digest};
 
 /// Simple lottery proof
 #[derive(Debug, Clone)]
 pub struct Proof {
     /// Sequence of elements from prover's set
     pub element_sequence: Vec<Element>,
+}
+
+fn lottery_hash(lottery_probability: f64, element: Element) -> bool {
+    let mut hasher = Blake2s256::new();
+    hasher.update(element);
+    let digest: Hash = hasher.finalize().into();
+    sample::sample_bernoulli(&digest, lottery_probability)
+}
+
+impl Proof {
+    /// Simple Lottery proving algorithm
+    pub fn new(params: &Params, prover_set: &[Element]) -> Option<Self> {
+        let mut element_sequence = Vec::with_capacity(params.proof_size as usize);
+        for &element in prover_set {
+            if lottery_hash(params.lottery_probability, element) {
+                element_sequence.push(element);
+            }
+            if prover_set.len() as u64 >= params.proof_size {
+                return Some(Proof { element_sequence });
+            }
+        }
+        None
+    }
+
+    /// Simple Lottery verifying algorithm
+    pub fn verify(&self, params: &Params) -> bool {
+        (self.element_sequence.len() as u64 == params.proof_size)
+            && self
+                .element_sequence
+                .iter()
+                .all(|&element| lottery_hash(params.lottery_probability, element))
+    }
 }
