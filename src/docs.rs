5f682656289898e6d1eef9fa59bd90f778db5f45
Tolik Zinovyev <aszinovyev@gmail.com>
2024-12-13 14:07:46 -0500

    Move various docs to one location. (#109)
    ## Content
    
    This PR moves various documentation to one location under `docs/` to
    avoid clutter.

--- src/docs.rs
+++ src/docs.rs
@@ -1,21 +1,21 @@
 //! Approximate Lower Bound Arguments (_ALBA_) documentation.
 
-#![doc = include_str!("../docs/intro.md")]
+#![doc = include_str!("../docs/rustdoc/intro.md")]
 
-#[doc = include_str!("../docs/varmap.md")]
+#[doc = include_str!("../docs/rustdoc/varmap.md")]
 pub mod variables {}
 
-#[doc = include_str!("../docs/basic_construction.md")]
+#[doc = include_str!("../docs/rustdoc/basic_construction.md")]
 pub mod basic {}
 
-#[doc = include_str!("../docs/centralized_telescope/main.md")]
+#[doc = include_str!("../docs/rustdoc/centralized_telescope/main.md")]
 pub mod centralized {
-    #[doc = include_str!("../docs/centralized_telescope/params.md")]
+    #[doc = include_str!("../docs/rustdoc/centralized_telescope/params.md")]
     pub mod params {}
 
-    #[doc = include_str!("../docs/centralized_telescope/proof.md")]
+    #[doc = include_str!("../docs/rustdoc/centralized_telescope/proof.md")]
     pub mod proof {}
 
-    #[doc = include_str!("../docs/centralized_telescope/round.md")]
+    #[doc = include_str!("../docs/rustdoc/centralized_telescope/round.md")]
     pub mod round {}
 }
