6bfa0e76f170ee60ff1ba7bb183dcb5a760c5dab
Raphael Toledo <raphael.toledo@iohk.io>
2025-01-21 16:46:00 +0000

    Benches/Centralized/ProofSize: Updating prints

--- benches/centralized_telescope/proof_size.rs
+++ benches/centralized_telescope/proof_size.rs
@@ -7,16 +7,28 @@ use utils::common::{
     criterion_helpers::centralized::BenchParam, test_vectors::centralized::ALL_TESTS,
 };
 
+fn print_bench(
+    soundness_param: &str,
+    completeness_param: &str,
+    set_size: &str,
+    lower_bound: &str,
+    proof_size: &str,
+) {
+    println!(
+        "{0: <23} | {1: <26} | {2: <14} | {3: <17} | {4: <14}",
+        soundness_param, completeness_param, set_size, lower_bound, proof_size
+    );
+}
+
 /// Function benchmarking the proof size
 fn proof_bench(params: &[BenchParam]) {
     println!("Centralized telescope -- proof size");
-    println!(
-        "{0: <23} | {1: <26} | {2: <14} | {3: <17} | {4: <14}",
+    print_bench(
         "soundness_param (λ_sec)",
         "completeness_param (λ_rel)",
         "set_size (n_p)",
         "lower_bound (n_f)",
-        "proof_size (u)"
+        "proof_size (u)",
     );
     for param in params {
         let parameters = Params::new(
@@ -25,13 +37,12 @@ fn proof_bench(params: &[BenchParam]) {
             param.set_size,
             param.lower_bound,
         );
-        println!(
-            "{0: <23} | {1: <26} | {2: <14} | {3: <17} | {4: <14}",
-            param.lambda_sec,
-            param.lambda_rel,
-            param.set_size,
-            param.lower_bound,
-            parameters.proof_size
+        print_bench(
+            param.lambda_sec.to_string().as_str(),
+            param.lambda_rel.to_string().as_str(),
+            param.set_size.to_string().as_str(),
+            param.lower_bound.to_string().as_str(),
+            parameters.proof_size.to_string().as_str(),
         );
     }
 }
