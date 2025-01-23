f5693ef36d6cb59103210e744f7b35bae3557666
Raphael Toledo <raphael.toledo@iohk.io>
2025-01-23 22:22:30 +0000

    Benches/TestVectors: Updating values to Peras only

--- benches/common/test_vectors.rs
+++ benches/common/test_vectors.rs
@@ -1,25 +1,25 @@
 //! Benchmark parameters
-//! The parameters chosen correspond to cases of interest for Cardano's Peras and Leios protocols.
+//! The parameters chosen correspond to cases of interest for Cardano's Peras protocol.
 
 pub mod centralized {
     use super::super::criterion_helpers::centralized::BenchParam;
 
     /// This case corresponds to the minimum security requirements with optimistic set_size
     const LOW_PARAM: BenchParam = BenchParam {
-        lambda_sec: 64.0,
-        lambda_rel: 64.0,
+        lambda_sec: 80.0,
+        lambda_rel: 80.0,
         total_num_elements: 1_000,
-        set_size: 900,
-        lower_bound: 600,
+        set_size: 950,
+        lower_bound: 750,
     };
 
     /// This case corresponds to medium security requirements with more realistic set_size
     const MID_PARAM: BenchParam = BenchParam {
-        lambda_sec: 80.0,
-        lambda_rel: 80.0,
+        lambda_sec: 128.0,
+        lambda_rel: 128.0,
         total_num_elements: 1_000,
-        set_size: 800,
-        lower_bound: 600,
+        set_size: 900,
+        lower_bound: 750,
     };
 
     /// This case corresponds to high security requirements with more realistic set_size
@@ -28,7 +28,7 @@ pub mod centralized {
         lambda_rel: 128.0,
         total_num_elements: 1_000,
         set_size: 800,
-        lower_bound: 670,
+        lower_bound: 750,
     };
 
     pub const SHORT_TESTS: &[BenchParam; 1] = &[LOW_PARAM];
