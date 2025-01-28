8e31542fdd350d46852311325dffc1c7203c6eb7
Raphael Toledo <raphael.toledo@iohk.io>
2025-01-28 16:50:52 +0000

    Tests/centralized: Update tests

--- tests/centralized_telescope.rs
+++ tests/centralized_telescope.rs
@@ -2,16 +2,22 @@
 
 use alba::centralized_telescope::Telescope;
 use alba::centralized_telescope::{params::Params, proof::Proof};
+use digest::{Digest, FixedOutput};
 use rand_chacha::ChaCha20Rng;
 use rand_core::{RngCore, SeedableRng};
 
 mod common;
 use common::gen_items;
 
+use blake2::Blake2bVar;
+use sha2::Sha256;
+
 const DATA_LENGTH: usize = 48;
 type Data = [u8; DATA_LENGTH];
 
-fn test(created_with_params: bool) {
+type P<H> = Proof<Data, H>;
+
+fn test<H: Digest + FixedOutput>(created_with_params: bool) {
     let mut rng = ChaCha20Rng::from_seed(Default::default());
     let nb_tests = 1_000;
     let nb_elements: u64 = 1_000;
@@ -21,67 +27,63 @@ fn test(created_with_params: bool) {
     let lower_bound = nb_elements.saturating_mul(20).div_ceil(100);
     for _t in 0..nb_tests {
         let seed = rng.next_u32().to_be_bytes().to_vec();
-        let s_p: Vec<Data> = gen_items::<DATA_LENGTH>(&seed, nb_elements);
+        let s_p: Vec<Data> = gen_items::<Blake2bVar, DATA_LENGTH>(&seed, nb_elements);
         let alba = if created_with_params {
             Telescope::create(soundness_param, completeness_param, set_size, lower_bound)
         } else {
             let setup = Params::new(soundness_param, completeness_param, set_size, lower_bound);
             Telescope::setup_unsafe(set_size, &setup)
         };
-        let proof = alba.prove(&s_p).unwrap();
+        let proof = alba.prove::<Data, H>(&s_p).unwrap();
         assert!(alba.verify(&proof));
         // Checking that the proof fails if proof.search_counter is erroneous
-        let proof_t = Proof {
-            retry_counter: proof.retry_counter,
-            search_counter: proof.search_counter.wrapping_add(1),
-            element_sequence: proof.element_sequence.clone(),
-        };
+        let proof_t = P::<H>::from(
+            proof.retry_counter,
+            proof.search_counter.wrapping_add(1),
+            proof.element_sequence.clone(),
+        );
         assert!(!alba.verify(&proof_t));
         // Checking that the proof fails if proof.retry_counter is erroneous
-        let proof_v = Proof {
-            retry_counter: proof.retry_counter.wrapping_add(1),
-            search_counter: proof.search_counter,
-            element_sequence: proof.element_sequence.clone(),
-        };
+        let proof_v = P::<H>::from(
+            proof.retry_counter.wrapping_add(1),
+            proof.search_counter,
+            proof.element_sequence.clone(),
+        );
         assert!(!alba.verify(&proof_v));
         // Checking that the proof fails when no elements are included
-        let proof_item = Proof {
-            retry_counter: proof.retry_counter,
-            search_counter: proof.search_counter,
-            element_sequence: Vec::<Data>::new(),
-        };
+        let proof_item = P::<H>::from(proof.retry_counter, proof.search_counter, Vec::new());
         assert!(!alba.verify(&proof_item));
         // Checking that the proof fails when wrong elements are included
         // We are trying to trigger proof_hash
         let mut wrong_items = proof.element_sequence.clone();
         let last_item = wrong_items.pop().unwrap();
         let mut penultimate_item = wrong_items.pop().unwrap();
-        let proof_itembis = Proof {
-            retry_counter: proof.retry_counter,
-            search_counter: proof.search_counter,
-            element_sequence: wrong_items.clone(),
-        };
+        let proof_itembis = P::<H>::from(
+            proof.retry_counter,
+            proof.search_counter,
+            wrong_items.clone(),
+        );
         assert!(!alba.verify(&proof_itembis));
         // Checking that the proof fails when wrong elements are included
         // We are trying to trigger round_hash
         penultimate_item[0] = penultimate_item[0].wrapping_add(42u8);
         wrong_items.push(penultimate_item);
         wrong_items.push(last_item);
-        let proof_itembis = Proof {
-            retry_counter: proof.retry_counter,
-            search_counter: proof.search_counter,
-            element_sequence: wrong_items.clone(),
-        };
+        let proof_itembis = P::<H>::from(
+            proof.retry_counter,
+            proof.search_counter,
+            wrong_items.clone(),
+        );
         assert!(!alba.verify(&proof_itembis));
     }
 }
 
 #[test]
 fn created_with_params() {
-    test(true);
+    test::<Sha256>(true);
 }
 
 #[test]
 fn created_with_setup() {
-    test(false);
+    test::<Sha256>(false);
 }
