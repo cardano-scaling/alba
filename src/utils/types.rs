99bf9d2311b4bebeb01d5cb94a922c1c1f0a0e9b
Raphael Toledo <raphael.toledo@iohk.io>
2025-02-03 17:12:46 +0000

    Utils/types: Adding proving error

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -1,3 +1,8 @@
+//! Types, traits and their implementation
+
+use std::error::Error;
+use std::fmt::{Display, Error as FmtError, Formatter};
+
 pub(crate) const DATA_LENGTH: usize = 48;
 
 /// Type of dataset's elements to lower bound
@@ -8,3 +13,27 @@ pub(crate) const DIGEST_SIZE: usize = 32;
 
 /// Hash type for internal hashes
 pub(crate) type Hash = [u8; DIGEST_SIZE];
+
+/// Proof generation error
+#[derive(Debug, Clone, Copy)]
+pub enum ProofGenerationError {
+    /// Proof cannot be guaranteed to be generated as not enough elements were submitted to the prover
+    NotEnoughElements,
+    /// No proof was found within bound
+    NotFoundInBounds,
+    /// No proof was found
+    NotFound,
+}
+
+impl Display for ProofGenerationError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
+        match self {
+            Self::NotEnoughElements => write!(f, "Not enough elements given to the prover"),
+            Self::NotFoundInBounds => write!(f, "No proof bound within DFs bound"),
+            Self::NotFound => write!(f, "No proof found"),
+        }
+    }
+}
+
+impl Error for ProofGenerationError {}
+
