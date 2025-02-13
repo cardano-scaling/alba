e03658eff5bd8abddda99fa7303ec4bceccc5c14
Raphael Toledo <raphael.toledo@iohk.io>
2025-02-13 12:28:53 +0000

    Centralized/Proof: Updating nb steps only afterwards

--- src/centralized_telescope/proof.rs
+++ src/centralized_telescope/proof.rs
@@ -188,9 +188,6 @@ impl<E: AsRef<[u8]> + Clone + Send + Sync, H: Digest + FixedOutput> Proof<E, H> 
                 }
                 // Initialize new round
                 if let Some(r) = Round::new(retry_counter, search_counter, set_size) {
-                    // Run DFS on such round, incrementing step
-                    Self::update_step_lock(step.clone(), params.proof_size);
-
                     // Initializing thread dependent steps
                     let search_step = Arc::new(Mutex::new(0u64));
 
@@ -203,11 +200,8 @@ impl<E: AsRef<[u8]> + Clone + Send + Sync, H: Digest + FixedOutput> Proof<E, H> 
                         proof_found.clone(),
                     );
 
-                    // Updating global steps with local dependent one if needs be
-                    let nb_steps = *search_step.lock().unwrap();
-                    if nb_steps != params.proof_size {
-                        Self::update_step_lock(step.clone(), nb_steps - params.proof_size);
-                    }
+                    // Updating global number of steps with the ones done in thread
+                    Self::update_step_lock(step.clone(), *search_step.lock().unwrap());
 
                     dfs_result
                 } else {
