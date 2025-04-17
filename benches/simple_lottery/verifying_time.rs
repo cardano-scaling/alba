6fe32b1ffb74aab67816e9e38203c27ef340e4d9
Raphael Toledo <raphael.toledo@iohk.io>
2025-04-17 18:30:14 +0100

    Applying lottery errors

--- benches/simple_lottery/verifying_time.rs
+++ benches/simple_lottery/verifying_time.rs
@@ -33,12 +33,12 @@ fn verify_duration<H: Digest + FixedOutput>(
     // Generate the proof
     let proof_opt = telescope.prove::<Data, H>(&dataset);
 
-    if let Some(proof) = proof_opt {
+    if let Ok(proof) = proof_opt {
         // Iterate on each sample `n` times
         for _ in 0..n {
             // Benching the verification time
             let start = Instant::now();
-            black_box(telescope.verify(&proof));
+            let _ = black_box(telescope.verify(&proof));
             total_duration = total_duration.saturating_add(start.elapsed());
         }
     }
